7611d885c112fb036bd7dd7279af4d59fae9be75
Rick C <rickcastaneda@outlook.com>
2020-11-04 00:31:02 -0600

    Introduce gitignore file and apply styling

--- src/callback/index.js
+++ src/callback/index.js
@@ -4,21 +4,21 @@ function sum(num1, num2) { // tenemos 1 func y le pasaremos otra
 
 //funcion que recibe como argumento 
 
-function calc(num1, num2,callback) {
+function calc(num1, num2, callback) {
     return callback(num1, num2)
 
-}  
-console.log(calc(2,2, sum))
+}
+console.log(calc(2, 2, sum))
 
 //trabajar con tiempos como vamos a esperar que sucedan
-function date(callback){
+function date(callback) {
     console.log(new Date)
-    setTimeout(function() {
-      let date = new Date
+    setTimeout(function () {
+        let date = new Date
     }, 3000)
 }
- function printDate (dateNow) {
-     console.log(dateNow)
- }
- date(printDate)
+function printDate(dateNow) {
+    console.log(dateNow)
+}
+date(printDate)
 
