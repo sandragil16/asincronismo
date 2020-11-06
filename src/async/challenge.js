aaeed35ab58b14f5382cb7ea136497c34a0161fa
Rick C <rickcastaneda@outlook.com>
2020-11-05 20:46:45 -0600

    Solve issue with parameter in Challenge.js

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -1,10 +1,10 @@
 const fetchData = require('../utils/fetchData.js')
 const API =  'https://rickandmortyapi.com/api/character/'
 
-const anotherFunction = async (url_api, ) => {
+const anotherFunction = async (url_api) => {
     try {
       const data = await fetchData(url_api)
-      const character = await fetchData (`${API}${data.results[0].id}`)
+      const character = await fetchData (`${url_api}${data.results[0].id}`)
       const origin = await fetchData(character.origin.url)
       console.log(data.info.count)
       console.log(character.name)
